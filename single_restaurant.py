32669fc4d07e76927572eddbb2f92da3e998ad71
Ashutosh Sharma <aattish2@gmail.com>
2021-01-03 19:21:23 +0530

    Made Pandas Dataframe

--- single_restaurant.py
+++ single_restaurant.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import pandas as pd
 url="https://www.foodpanda.my/chain/cb0hc/domino-s-pizza-pos-integration"
 html =  requests.get(url)
 
@@ -16,17 +17,13 @@ image_url=[]
 
 for data in item:
     h2=data.find('h2',class_='dish-category-title').text.strip()
-    #print('h2=',h2)
-    # para=data.find('p',class_='dish-category-description').text.strip()
     titles=data.find_all('h3',class_='dish-name fn p-name')
     for title in titles:
         dish_category_title.append(h2)
         item=title.text.strip()
-        #print('title=',item)
         dish_name.append(item)
         
-   
-    # photo=data.find('div',class_='photo u-photo b-lazy b-loaded')['data-src']
+
     prices=data.find_all('span',class_='price p-price')
     for price in prices:
         price=str(price)
@@ -34,14 +31,11 @@ for data in item:
         dis_price=price[-4]
         org_price=price[-2]
         org_price=(((org_price.split('>'))[1]).split('<'))[0]
-        #print('dis_price=', end="")
         try:
             x=price.index('from')
-            #print(' from ', end="")
             dis_price="from"+dis_price
         except:
             pass
-        #print(dis_price, ',  org_price=',org_price)
         original_price.append(org_price)
         discounted_price.append(dis_price)
         
@@ -50,17 +44,17 @@ for data in item:
         img=str(img)
         img=((img.split())[5]).split('=')
         img=(((img[1])[1:]).split('?'))[0]
-        #print(img)
         image_url.append(img)
 
+# now lists are made for the categories
+#we will now use the lists as pandas series and create a pandas dataframe
+
+df=pd.DataFrame({'dish_category_title':dish_category_title,
+'dish_name':dish_name,
+'original_price':original_price,
+'discounted_price':discounted_price,
+'image_url':image_url})
+
+print(df.head())
 
 
-print(dish_category_title)
-print()
-print(dish_name)
-print()
-print(original_price)
-print()
-print(discounted_price)
-print()
-print(image_url)
